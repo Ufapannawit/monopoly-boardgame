a4798a904041c91a031b58e3399b55665de5c396
Pannawit <pannawit.p@ku.th>
2022-08-19 18:56:17 +0700

    first commit

---
+++ monopoly/src/monopoly/Piece.java
@@ -0,0 +1,14 @@
+package monopoly;
+
+public class Piece {
+    private Square location;
+
+    public void setLocation(Square location) {
+        this.location = location;
+    }
+
+    public Square getLocation() {
+        return location;
+
+    }
+}
---
+++ monopoly/src/monopoly/Square.java
@@ -0,0 +1,9 @@
+package monopoly;
+
+public class Square {
+
+    private String name;
+
+
+
+}
